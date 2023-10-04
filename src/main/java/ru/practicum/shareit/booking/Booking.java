d72afb7ab6b7446654d6e55c6b39a6fd268cf434
melentyevSergey <melentyev.sergey@yandex.ru>
2023-10-05 00:07:53 +0300

    DB setup

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import ru.practicum.shareit.utils.BookingStatus;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
 public class Booking {
+    private Integer id;
+    private Instant start;
+    private Instant end;
+    private Integer itemId;
+    private Integer bookerId;
+    private BookingStatus status;
 }
---
+++ src/main/java/ru/practicum/shareit/utils/BookingStatus.java
@@ -0,0 +1,8 @@
+package ru.practicum.shareit.utils;
+
+public enum BookingStatus {
+    WAITING,
+    APPROVED,
+    REJECTED,
+    CANCELED
+}
