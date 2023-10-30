b89351bcf479445f3e478f31cab04fc0e9170293
melentyevSergey <melentyev.sergey@yandex.ru>
2023-10-30 21:18:58 +0300

    Финальные правки

--- src/main/java/ru/practicum/shareit/comment/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/comment/CommentRepository.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.comment;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.comment.Comment;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
